c8d1b32c7de8c0b87f2e87db4b07cae8cd0180d7
Joe McMahon <joe.mcmahon@gmail.com>
2009-08-24 00:02:07 -0700

    release 0.07 - fix license and META.yml

--- lib/URI/ImpliedBase.pm
+++ lib/URI/ImpliedBase.pm
@@ -6,7 +6,7 @@ use URI;
 BEGIN {
 	use Exporter ();
 	use vars qw ($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS);
-	$VERSION     = 0.06;
+	$VERSION     = 0.07;
 	@ISA         = qw (Exporter URI);
 	#Give a hoot don't pollute, do not export more than needed by default
 	@EXPORT      = qw ();
